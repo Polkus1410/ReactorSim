ec005d61991e3711953d3839eb7c5b4fc95e3282
4a <4a@3SK-5>
2025-03-20 09:33:44 +0100

    Dodaj pliki projektów.

---
+++ ReactorSim/Views/SimulationDrawable.cs
@@ -0,0 +1,20 @@
+﻿using ReactorSim.Models;
+
+namespace ReactorSim.Views
+{
+    public class SimulationDrawable : IDrawable
+    {
+      
+        public EntitysList EntitysList { get; set; }
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        { 
+            canvas.StrokeColor = Colors.Blue;
+            canvas.StrokeSize = 6;
+            foreach (var neutron in EntitysList.neutronList)
+            {
+                canvas.DrawCircle(new PointF(neutron.x_pos, neutron.y_pos), 10);
+            }
+            //canvas.DrawCircle(new PointF(EntityList, 100), 30);
+        }
+    }
+}
